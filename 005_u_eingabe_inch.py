e4c6b4faaa8fa0641a3a3ff4fadad034791ca121
rn-ArchiCAT <rn-mail@posteo.de>
2022-07-03 09:50:34 +0200

    004-005

---
+++ 005_u_eingabe_inch.py
@@ -0,0 +1,13 @@
+#Eingabe
+print("Bitte geben Sie einen Inch(Zoll-Wert) ein:")
+
+#Variablen
+a=input()
+a_float=float(a)
+b=2.54
+
+#Berechnung
+c=a_float*b
+
+#Ausgabe
+print("Das sind Zentimter:",c)
