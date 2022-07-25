7a6fcdd091e66e181e9e0e5cf8458f92cc23f917
rn-ArchiCAT <rn-mail@posteo.de>
2022-07-25 22:21:25 +0200

    017-021

---
+++ 018_Datei schleife_for.py
@@ -0,0 +1,3 @@
+#Schleife
+for i in 2, 7.5, -22:
+    print("Zahl:",i,", Quadrant:",i*i)
---
+++ 019_Datei schleife_break.py
@@ -0,0 +1,8 @@
+#Schleife
+for i in 12, -4, -20, 7:
+    if i*i > 200:
+        break
+    print("Zahl:",i,", Quadrant:",i*i)
+
+#Normale Ausgabe
+print("Ende")
