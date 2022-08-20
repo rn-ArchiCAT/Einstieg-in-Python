382634c3a7c4fa64214d0e34759da92db9851cc5
rn-ArchiCAT <rn-mail@posteo.de>
2022-08-20 11:08:44 +0200

    047-050

---
+++ 047_Datei zahl_ganz.py
@@ -0,0 +1,9 @@
+a = 27
+print("Dezimal:",a)
+print("ganze Zahl",int(a))
+print("Hexadezimal:",hex(a))
+print("Oktal:",oct(a))
+print("Dual:",bin(a))
+
+b = 27 + 27 + 0x1b + 0o33 + 0b11011
+print("Summe:",b)
