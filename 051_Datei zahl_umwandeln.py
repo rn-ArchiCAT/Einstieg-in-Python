a5b5f7c72340d9b29082bd0b10bd88fb37c98c7f
rn-ArchiCAT <rn-mail@posteo.de>
2022-08-21 16:52:55 +0200

    051-052

---
+++ 051_Datei zahl_umwandeln.py
@@ -0,0 +1,29 @@
+# Positive zahl
+x = 12/7
+print("x",x)
+
+# Rundung und Konvertierung
+rx = round(x,3)
+print("x gerundet auf drei Stellen:", rx)
+
+rx = round(x)
+print("x gerundet auf null Stellen:", rx)
+
+ix = int(x)
+print("int(x):", ix)
+print()
+
+# Negative zahl
+x = -12/7
+print("x",x)
+
+# Rundung und Konvertierung
+rx = round(x,3)
+print("x gerundet auf drei Stellen:", rx)
+
+rx = round(x)
+print("x gerundet auf null Stellen:", rx)
+
+ix = int(x)
+print("int(x):", ix)
+print()
